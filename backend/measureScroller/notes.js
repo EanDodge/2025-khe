19933117368aebdf1de0558d027ac2a025f81e0f
Ben <150054499+Beleebs@users.noreply.github.com>
2025-02-15 18:41:05 -0500

    added p5.js files

---
+++ backend/measureScroller/notes.js
@@ -0,0 +1,18 @@
+// Note Class
+
+class Note {
+  
+  // pitch is a string with the note value ("Eb", "B", etc.)
+  // length is a string with a char describing the length
+  // for example, w would be whole note, h=half note,
+  // q=quarter note, etc.
+  
+  constructor(pitch, length) {
+    this.pitch = pitch;
+    this.length = length;
+  }
+  
+  // kljdfhkjhfdfads
+  
+  
+}
