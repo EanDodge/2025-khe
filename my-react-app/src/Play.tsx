865a2a63f47eb772dc32626cc5508d1f3bfb821e
EanDodge <145071390+EanDodge@users.noreply.github.com>
2025-02-15 22:50:52 -0500

    update

--- my-react-app/src/Play.tsx
+++ my-react-app/src/Play.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import saxLogo from './assets/saximage2.webp'
+import saxLogo1 from '../../Mockups/SaxBlackWhite.png'
 import './Play.css'
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <div className="logo">
+      <div className="logo1">
         <Link to = "/">
-        <img src={saxLogo} alt="Saxaphone" />
+        <img src={saxLogo1} alt="Saxaphone" />
         </Link>
 
       </div>
@@ -25,7 +25,7 @@ function Play() {
     <>
       <title>Navbar with p5.js Sketch</title>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
-      <script src="sketch.js"></script>
+      <script src="../../backend/SaxophoneController/sketch.js"></script>
 
       <body>
         <NavBar />
