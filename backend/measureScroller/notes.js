f30c82d6195ef91ba3fdf690ad3cda6e8089ecb8
Ben <150054499+Beleebs@users.noreply.github.com>
2025-02-16 07:36:33 -0500

    measures. finally.

--- backend/measureScroller/notes.js
+++ backend/measureScroller/notes.js
@@ -26,20 +26,20 @@ const pitchValues = {
 }
 
 class Note {
-  
+
   // pitch is a string with the note value ("Eb", "B", etc.)
   // length is a string with a char describing the length
   // for example, w would be whole note, h=half note,
   // q=quarter note, etc.
 
   // put in .wav files here
-  
+
   constructor(pitch, length) {
     this.pitch = pitch;
     this.length = length;
     this.isRest = false;
   }
-  
+
   info() {
     let returnString = "";
     if (this.pitch == "R") {
@@ -87,7 +87,7 @@ class Measure {
       this.measureValue -= noteValues[removed.length];
     }
   }
-  
+
   debug() {
     let returnString = "This measure contains: "
     for (let i = 0; i < this.notes.length; i++) {
@@ -100,207 +100,211 @@ class Measure {
   length() {
     return this.notes.length;
   }
-  
+
 }
 
 class Song {
-  constructor() {
+  constructor(song, bpm) {
     this.measures = [];
-    this.title = "New Song";
-    this.bpm = 120;
+    this.title = song;
+    this.bpm = bpm;
   }
 
   addMeasure(measure) {
     this.measures.push(measure);
   }
-  
+
   length() {
     return this.measures.length;
   }
-  
+
   debugSong() {
     for (let i = 0; i < this.measures.length; i++) {
       console.log("measure #" + i + ": " + this.measures[i].debug());
     }
   }
+
+  currentMeasure() {
+
+  }
 }
 
+class Receptor {
+  constructor() {
+    this.exists = true;
+  }
+
+  display() {
+    line(100, 30, 100, 190);
+  }
+
+
+}
+
+
+
+
 // p5.js visualization
+let wholeNote;
+let openNote;
+let closedNote;
+
 
 let testSong;
 let testMeasure1, testMeasure2, testMeasure3, testMeasure4;
-let testNote1, testNote2, testNote3, testNote4, testNote5;
+
+// TODO
+//  Implement scrollSpeed in Song and draw
+//  Implement a receptor line
+//  Implement noteLength
+//
+//
+
+function preload() {
+  wholeNote = loadImage('noteWhole.png');
+  openNote = loadImage('noteOpen.png');
+  closedNote = loadImage('noteClosed.png');
+}
+
+let scrollX = 0;
+let scrollSpeed;
+let measureWidth = 300;
+let canvasWidth;
 
 function setup() {
-  testNote1 = new Note("B", "q");
-  testNote2 = new Note("A", "q");
-  testNote3 = new Note("G", "h");
-  testNote4 = new Note("G", "e");
-  testNote5 = new Note("A", "e");
 
+  // initialize measures
   testMeasure1 = new Measure();
   testMeasure2 = new Measure();
   testMeasure3 = new Measure();
   testMeasure4 = new Measure();
 
-  testSong = new Song();
+  // initialize song
+  testSong = new Song("Hot Cross Buns", 60);
 
   // my song :)
   // hot cross buns
 
   // add notes
-  testMeasure1.addNote(new Note("Db", 'e'));
-  testMeasure1.addNote(new Note("C", 'e'));
-  testMeasure1.addNote(new Note("B", 'e'));
-  testMeasure1.addNote(new Note("Bb", 'e'));
-  testMeasure1.addNote(new Note("A", 'e'));
-  testMeasure1.addNote(new Note("Ab", 'e'));
-  testMeasure1.addNote(new Note("G", 'e'));
-  testMeasure1.addNote(new Note("Gb", 'e'));
+  testMeasure1.addNote(new Note("B", 'q'));
+  testMeasure1.addNote(new Note("A", 'q'));
+  testMeasure1.addNote(new Note("G", 'h'));
 
-  testMeasure2.addNote(new Note("Db", 'e'));
-  testMeasure2.addNote(new Note("R", 'e'));
-  testMeasure2.addNote(new Note("B", 'e'));
-  testMeasure2.addNote(new Note("Bb", 'e'));
-  testMeasure2.addNote(new Note("A", 'e'));
-  testMeasure2.addNote(new Note("Ab", 'e'));
-  testMeasure2.addNote(new Note("G", 'e'));
-  testMeasure2.addNote(new Note("Gb", 'e'));
+  testMeasure2.addNote(new Note("B", 'q'));
+  testMeasure2.addNote(new Note("A", 'q'));
+  testMeasure2.addNote(new Note("G", 'h'));
 
-  testMeasure3.addNote(new Note("Db", 'e'));
-  testMeasure3.addNote(new Note("R", 'e'));
-  testMeasure3.addNote(new Note("B", 'e'));
-  testMeasure3.addNote(new Note("Bb", 'e'));
-  testMeasure3.addNote(new Note("A", 'e'));
-  testMeasure3.addNote(new Note("Ab", 'e'));
   testMeasure3.addNote(new Note("G", 'e'));
-  testMeasure3.addNote(new Note("Gb", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
 
-  /*
-  testMeasure2.addNote(testNote1);
-  testMeasure2.addNote(testNote2);
-  testMeasure2.addNote(testNote3);
+  testMeasure4.addNote(new Note("B", 'q'));
+  testMeasure4.addNote(new Note("A", 'q'));
+  testMeasure4.addNote(new Note("G", 'h'));
 
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-
-  testMeasure4.addNote(testNote1);
-  testMeasure4.addNote(testNote2);
-  testMeasure4.addNote(testNote3);
-  */
-  
   // add measures
   testSong.addMeasure(testMeasure1);
   testSong.addMeasure(testMeasure2);
   testSong.addMeasure(testMeasure3);
-  // testSong.addMeasure(testMeasure4);
-  
+  testSong.addMeasure(testMeasure4);
 
-  // creates canvas size based on however many measures there are
-  createCanvas(400 * testSong.length(), 400); 
-  testSong.debugSong();
+  // Fixed canvas width to allow for scrolling across the screen
+  canvasWidth = 800;
+  createCanvas(canvasWidth, 400);
+  scrollX = testSong.length() * measureWidth; // Start off-screen
+  imageMode(CENTER);
+
+  // bps * default scroll speed / 4
+  scrollSpeed = (testSong.bpm / 60) * 5 / 4;
 }
+
+// just for the game, i will always have this as true
+// i FOR THE LIFE OF ME could not implement the scrolling functionality in draw.
+let autoScroll = true;
 
 function draw() {
-  // draw song
-  // ...somehow
   background(220);
 
-  // draw staff
-  for (let i = 0; i < 5; i++) {
-    line(50, 50 + i * 30, testSong.length() * 300 + 50, 50 + i * 30);
-  }
-  
-  // draw measure lines
-  for (let i = 0; i < testSong.length() + 1; ++i) {
-    line(i * 300 + 50, 50, i * 300 + 50, 170);
-  }
+  // Scroll the song to the left over time
+  if (autoScroll) {
+    scrollX -= scrollSpeed;
 
-  
-  // draw j notes in i measures
-  let x = 50;
-  for (let i = 0; i < testSong.length(); ++i) {
-    for (let j = 0; j < testSong.measures[i].length(); ++j) {
-      let flat = false;
-      let sharp = false;
-      let rest = false;
-
-      // note length is used to calculate where it is spacing wise in the measure,
-      // note pitch is used to determine which line it appears on
-      let noteLength = noteValues[testSong.measures[i].notes[j].length];
-      let notePitch = pitchValues[testSong.measures[i].notes[j].pitch];
-
-      // checks for flat/sharp
-      // changes position to the natural counterpart
-      switch(notePitch) {
-        case -1:
-          rest = true;
-          notePitch = 2;
-          break;
-        case 0.5: // Db or C#
-          sharp = true;
-          flat = false;
-          notePitch = 1;
-          break;
-        case 2.5: // Bb
-          sharp = false;
-          flat = true;
-          notePitch = 2;
-          break;
-        case 3.5: // Ab or G#
-          sharp = true;
-          flat = false;
-          notePitch = 4;
-          break;
-        case 4.5: // Gb or F#
-          sharp = true;
-          flat = false;
-          notePitch = 5;
-          break;  
-        case 6.5: // Eb
-          sharp = false;
-          flat = true;
-          notePitch = 6;
-          break;
-        default: // anything else
-          sharp = false;
-          flat = false;
-      }
-
-      // calculates note placement
-      // z represents the distance between the amount of notes in each measure
-      let z = 300 / testSong.measures[i].length();
-      let y = 15 * notePitch + 80;
-
-      // if flat, make it red
-      // if sharp make it blue
-      // else, regular color
-      if (flat) {
-        fill(255, 0, 0);
-        circle(x, y, 30);
-      }
-      else if (sharp) {
-        fill(0, 0, 255);
-        circle(x, y, 30);
-      }
-      else if (rest) {
-        fill(0);
-        circle(x, y, 30);
-      }
-      else {
-        fill(255);
-        circle(x, y, 30);
-      }
-      
-      console.log("placed note #" + j);
-      console.log("placed circle at: " + (x + 50) + ", " + (y + 25));
-      x += z;                                                                                                                                                                              
+    if (scrollX < -testSong.length * measureWidth) {  
+      autoScroll = false;
+      scrollX = -testSong.length * measureWidth;
     }
   }
+
+  // Draw the staff lines
+  for (let i = 0; i < 5; i++) {
+    line(50, 50 + i * 30, canvasWidth - 50, 50 + i * 30);
+  }
+
+  let currentX = 50 + scrollX; // Adjust for scrolling
+
+  for (let i = 0; i < testSong.length(); i++) {
+    let measure = testSong.measures[i];
+
+    // Draw measure lines
+    line(currentX, 50, currentX, 170);
+
+    let measureStartX = currentX;
+    let currentBeatPosition = 0;
+
+    for (let j = 0; j < measure.notes.length; j++) {
+      let note = measure.notes[j];
+      let noteDuration = noteValues[note.length];
+      let noteWidth = (noteDuration / 4) * measureWidth;
+      let xPos = measureStartX + (currentBeatPosition / 4) * measureWidth;
+      let yPos;
+
+      if (note.pitch === "R") {
+        // Rest positioning
+        yPos = 110;
+        fill(0);
+        rect(xPos + noteWidth / 4, yPos - 5, noteWidth / 2, 10);
+      } else {
+        // Get vertical positioning for notes
+        let notePitch = pitchValues[note.pitch];
+        let flat = false, sharp = false;
+
+        switch (notePitch) {
+          case 0.5: sharp = true; notePitch = 1; break;
+          case 2.5: flat = true; notePitch = 2; break;
+          case 3.5: sharp = true; notePitch = 4; break;
+          case 4.5: sharp = true; notePitch = 5; break;
+          case 6.5: flat = true; notePitch = 6; break;
+        }
+
+        yPos = 15 * notePitch + 80; // Convert pitch to y-position
+
+        // Draw note image
+        let noteImage;
+        switch (note.length) {
+          case "w": noteImage = wholeNote; break;
+          case "h": noteImage = openNote; break;
+          case "q": 
+          case "e": 
+          case "s": noteImage = closedNote; break;
+        }
+
+        image(noteImage, xPos + noteWidth / 2, yPos);
+      }
+
+      // Move to the next beat
+      currentBeatPosition += noteDuration;
+    }
+
+    // Move to the next measure
+    currentX += measureWidth;
+  }
+
+  // Draw final measure line
+  line(currentX, 50, currentX, 170);
 }
+
--- my-react-app/backend/measureScroller/notes.js
+++ my-react-app/backend/measureScroller/notes.js
@@ -26,20 +26,20 @@ const pitchValues = {
 }
 
 class Note {
-  
+
   // pitch is a string with the note value ("Eb", "B", etc.)
   // length is a string with a char describing the length
   // for example, w would be whole note, h=half note,
   // q=quarter note, etc.
 
   // put in .wav files here
-  
+
   constructor(pitch, length) {
     this.pitch = pitch;
     this.length = length;
     this.isRest = false;
   }
-  
+
   info() {
     let returnString = "";
     if (this.pitch == "R") {
@@ -87,7 +87,7 @@ class Measure {
       this.measureValue -= noteValues[removed.length];
     }
   }
-  
+
   debug() {
     let returnString = "This measure contains: "
     for (let i = 0; i < this.notes.length; i++) {
@@ -100,207 +100,222 @@ class Measure {
   length() {
     return this.notes.length;
   }
-  
+
 }
 
 class Song {
-  constructor() {
+  constructor(song, bpm) {
     this.measures = [];
-    this.title = "New Song";
-    this.bpm = 120;
+    this.title = song;
+    this.bpm = bpm;
   }
 
   addMeasure(measure) {
     this.measures.push(measure);
   }
-  
+
   length() {
     return this.measures.length;
   }
-  
+
   debugSong() {
     for (let i = 0; i < this.measures.length; i++) {
       console.log("measure #" + i + ": " + this.measures[i].debug());
     }
   }
+
+  currentMeasure() {
+
+  }
 }
+
+class Receptor {
+  constructor() {
+    this.exists = true;
+  }
+
+  display() {
+    line(100, 30, 100, 190);
+  }
+
+
+}
+
+
+
 
 // p5.js visualization
 
 let testSong;
 let testMeasure1, testMeasure2, testMeasure3, testMeasure4;
-let testNote1, testNote2, testNote3, testNote4, testNote5;
+
+// TODO
+//  Implement scrollSpeed in Song and draw
+//  Implement a receptor line
+//  Implement noteLength
+//
+//
+
+function preload() {
+  
+}
+
+let scrollX = 0;
+let scrollSpeed = 5;
+let measureWidth = 300;
+let canvasWidth;
 
 function setup() {
-  testNote1 = new Note("B", "q");
-  testNote2 = new Note("A", "q");
-  testNote3 = new Note("G", "h");
-  testNote4 = new Note("G", "e");
-  testNote5 = new Note("A", "e");
 
+  // initialize measures
   testMeasure1 = new Measure();
   testMeasure2 = new Measure();
   testMeasure3 = new Measure();
   testMeasure4 = new Measure();
 
-  testSong = new Song();
+  // initialize song
+  testSong = new Song("Hot Cross Buns", 60);
 
   // my song :)
   // hot cross buns
 
   // add notes
-  testMeasure1.addNote(new Note("Db", 'e'));
-  testMeasure1.addNote(new Note("C", 'e'));
-  testMeasure1.addNote(new Note("B", 'e'));
-  testMeasure1.addNote(new Note("Bb", 'e'));
-  testMeasure1.addNote(new Note("A", 'e'));
-  testMeasure1.addNote(new Note("Ab", 'e'));
-  testMeasure1.addNote(new Note("G", 'e'));
-  testMeasure1.addNote(new Note("Gb", 'e'));
+  testMeasure1.addNote(new Note("B", 'q'));
+  testMeasure1.addNote(new Note("A", 'q'));
+  testMeasure1.addNote(new Note("G", 'h'));
 
-  testMeasure2.addNote(new Note("Db", 'e'));
-  testMeasure2.addNote(new Note("C", 'e'));
-  testMeasure2.addNote(new Note("B", 'e'));
-  testMeasure2.addNote(new Note("Bb", 'e'));
-  testMeasure2.addNote(new Note("A", 'e'));
-  testMeasure2.addNote(new Note("Ab", 'e'));
-  testMeasure2.addNote(new Note("G", 'e'));
-  testMeasure2.addNote(new Note("Gb", 'e'));
+  testMeasure2.addNote(new Note("B", 'q'));
+  testMeasure2.addNote(new Note("A", 'q'));
+  testMeasure2.addNote(new Note("G", 'h'));
 
-  testMeasure3.addNote(new Note("Db", 'e'));
-  testMeasure3.addNote(new Note("C", 'e'));
-  testMeasure3.addNote(new Note("B", 'e'));
-  testMeasure3.addNote(new Note("Bb", 'e'));
-  testMeasure3.addNote(new Note("A", 'e'));
-  testMeasure3.addNote(new Note("Ab", 'e'));
   testMeasure3.addNote(new Note("G", 'e'));
-  testMeasure3.addNote(new Note("Gb", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("G", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
+  testMeasure3.addNote(new Note("A", 'e'));
 
-  /*
-  testMeasure2.addNote(testNote1);
-  testMeasure2.addNote(testNote2);
-  testMeasure2.addNote(testNote3);
+  testMeasure4.addNote(new Note("B", 'q'));
+  testMeasure4.addNote(new Note("A", 'q'));
+  testMeasure4.addNote(new Note("G", 'h'));
 
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote4);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-  testMeasure3.addNote(testNote5);
-
-  testMeasure4.addNote(testNote1);
-  testMeasure4.addNote(testNote2);
-  testMeasure4.addNote(testNote3);
-  */
-  
   // add measures
   testSong.addMeasure(testMeasure1);
   testSong.addMeasure(testMeasure2);
   testSong.addMeasure(testMeasure3);
-  // testSong.addMeasure(testMeasure4);
-  
+  testSong.addMeasure(testMeasure4);
 
-  // creates canvas size based on however many measures there are
-  createCanvas(400 * testSong.length(), 400); 
-  testSong.debugSong();
+  // Fixed canvas width to allow for scrolling across the screen
+  canvasWidth = 800;
+  createCanvas(canvasWidth, 400);
+  scrollX = testSong.length() * measureWidth; // Start off-screen
 }
 
+// just for the game, i will always have this as true
+// i couldn't find a workaround to pause in debugging, but i don't even know if it works
+// so i opted to have the scroll stop once it was out of the measure bounds.
+let autoScroll = true;
+
 function draw() {
-  // draw song
-  // ...somehow
   background(220);
 
-  // draw staff
-  for (let i = 0; i < 5; i++) {
-    line(50, 50 + i * 30, testSong.length() * 300 + 50, 50 + i * 30);
-  }
-  
-  // draw measure lines
-  for (let i = 0; i < testSong.length() + 1; ++i) {
-    line(i * 300 + 50, 50, i * 300 + 50, 170);
-  }
+  // if we have autoscroll enabled,
+  // the scrollX is constantly being moved by the scrollSpeed
+  if (autoScroll) {
+    scrollX -= scrollSpeed;
 
-  
-  // draw j notes in i measures
-  let x = 50;
-  for (let i = 0; i < testSong.length(); ++i) {
-    for (let j = 0; j < testSong.measures[i].length(); ++j) {
-      let flat = false;
-      let sharp = false;
-      let rest = false;
-
-      // note length is used to calculate where it is spacing wise in the measure,
-      // note pitch is used to determine which line it appears on
-      let noteLength = noteValues[testSong.measures[i].notes[j].length];
-      let notePitch = pitchValues[testSong.measures[i].notes[j].pitch];
-
-      // checks for flat/sharp
-      // changes position to the natural counterpart
-      switch(notePitch) {
-        case -1:
-          rest = true;
-          notePitch = 2;
-          break;
-        case 0.5: // Db or C#
-          sharp = true;
-          flat = false;
-          notePitch = 1;
-          break;
-        case 2.5: // Bb
-          sharp = false;
-          flat = true;
-          notePitch = 2;
-          break;
-        case 3.5: // Ab or G#
-          sharp = true;
-          flat = false;
-          notePitch = 4;
-          break;
-        case 4.5: // Gb or F#
-          sharp = true;
-          flat = false;
-          notePitch = 5;
-          break;  
-        case 6.5: // Eb
-          sharp = false;
-          flat = true;
-          notePitch = 6;
-          break;
-        default: // anything else
-          sharp = false;
-          flat = false;
-      }
-
-      // calculates note placement
-      // z represents the distance between the amount of notes in each measure
-      let z = 300 / testSong.measures[i].length();
-      let y = 15 * notePitch + 80;
-
-      // if flat, make it red
-      // if sharp make it blue
-      // else, regular color
-      if (flat) {
-        fill(255, 0, 0);
-        circle(x, y, 30);
-      }
-      else if (sharp) {
-        fill(0, 0, 255);
-        circle(x, y, 30);
-      }
-      else if (rest) {
-        fill(0);
-        circle(x, y, 30);
-      }
-      else {
-        fill(255);
-        circle(x, y, 30);
-      }
-      
-      console.log("placed note #" + j);
-      console.log("placed circle at: " + (x + 50) + ", " + (y + 25));
-      x += z;                                                                                                                                                                              
+    // checks to see if we have completely scrolled through the song
+    if (scrollX < -testSong.length() * measureWidth) {
+      autoScroll = false;
+      scrollX = -testSong.length() * measureWidth;
     }
   }
+
+  // Draw staff lines
+  for (let i = 0; i < 5; i++) {
+    line(50, 50 + i * 30, canvasWidth - 50, 50 + i * 30);
+  }
+
+  // currentX is used
+  let currentX = 50 + scrollX;
+
+  for (let i = 0; i < testSong.length(); i++) {
+    // Measure lines
+    line(currentX, 50, currentX, 170);
+
+    for (let j = 0; j < testSong.measures[i].length(); j++) {
+      let note = testSong.measures[i].notes[j];
+      let z = measureWidth / testSong.measures[i].length();
+      let y;
+
+      // Handles whether or not the note is a rest. Places a separate rest icon
+      if (note.pitch === "R") {
+        y = 100;
+        fill(0);
+        rect(currentX + j * z + z / 4, y - 5, z / 2, 10);
+        console.log("placed rest #" + j);
+      }
+      else {
+        let flat = false;
+        let sharp = false;
+        let notePitch = pitchValues[note.pitch];
+
+        switch (notePitch) {
+          case 0.5: // Db or C#
+            sharp = true;
+            flat = false;
+            notePitch = 1;
+            break;
+          case 2.5: // Bb
+            sharp = false;
+            flat = true;
+            notePitch = 2;
+            break;
+          case 3.5: // Ab or G#
+            sharp = true;
+            flat = false;
+            notePitch = 4;
+            break;
+          case 4.5: // Gb or F#
+            sharp = true;
+            flat = false;
+            notePitch = 5;
+            break;
+          case 6.5: // Eb
+            sharp = false;
+            flat = true;
+            notePitch = 6;
+            break;
+          default: // anything else
+            sharp = false;
+            flat = false;
+        }
+        // get note position on staff
+        y = 15 * notePitch + 80;
+
+        // handles note placement
+        // if flat, red
+        // if sharp, blue
+        // if natural, white
+        if (flat) {
+          fill(255, 0, 0);
+          circle(currentX + j * z + z / 2, y, 30);
+        }
+        else if (sharp) {
+          fill(0, 0, 255);
+          circle(currentX + j * z + z / 2, y, 30);
+        }
+        else {
+          fill(255);
+          circle(currentX + j * z + z / 2, y, 30);
+        }
+        console.log("placed note #" + j);
+        console.log("placed circle at: " + (currentX + j * z + z / 2) + ", " + y);
+      }
+    }
+    currentX += measureWidth;
+  }
+  line(currentX, 50, currentX, 170);
 }
